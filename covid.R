121452a4387776a221d0a3a32bec94a1b33936fe
Nicholas Coxen <nicjcoxen@gmail.com>
2022-03-19 16:21:24 +1300

    facet

--- covid.R
+++ covid.R
@@ -22,17 +22,17 @@ data<-data.frame(data)
 
 data2<-data[complete.cases(data), ]
 
-names(data)
 
 df<-data %>% select(location,iso_code,continent,date,total_cases,total_deaths,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,stringency_index,population) %>%
-  arrange(desc(stringency_index))
-
+  arrange(desc(stringency_index))%>%
+  mutate(month = format(date, "%m"), year = format(date, "%Y")) %>%
+  group_by(month, year)
 
 write.csv(df,"covid.csv",row.names=FALSE)
 
 skimr::skim(df)
 
 
-GGally(df)
+ggplot(df, aes(date,people_fully_vaccinated,color = year)) + geom_jitter() + facet_wrap(~  year)  + theme_bw() + labs(title = "Vaccination rate (Globally)",subtitle = "Coronavirus vaccinations by year", y = "People fully vaccinated")
 
-ggsave("covariance_matrix.png", width = 7, height = 7)
+ggsave("Vaccination_facet.png", width = 10, height = 7)
